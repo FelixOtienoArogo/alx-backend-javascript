c8673b7ed2acaba72f02bcd5642cf75047ceab77
FelixOtienoArogo <felixarogo@gmail.com>
2023-04-19 14:57:00 +0300

    default strings

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
   const promise = uploadPhoto(fileName);
   const promise1 = signUpUser(firstName, lastName);
   return Promise.allSettled([promise, promise1]);
