515b3f6b141a132cf29e05217341d40181aa10fb
FelixOtienoArogo <felixarogo@gmail.com>
2023-04-19 16:14:48 +0300

    null on error

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,9 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-
+  let photo;
+  let user;
+  try {
+    user = await createUser();
+    photo = await uploadPhoto();
+  } catch (error) {
+    photo = null;
+    user = null;
+  }
   return ({
     photo,
     user,
