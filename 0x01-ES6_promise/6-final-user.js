c763e1b8040ea6d4dcca712dc2ad3bf457bc3ab9
FelixOtienoArogo <felixarogo@gmail.com>
2023-04-20 12:29:23 +0300

    reformat 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,32 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+    const photo = {
+	status: 'pending',
+	value: '',
+    }
+    const user = {
+	status: 'pending',
+	value: '',
+    }
+
+    try {
+	const upload =	await uploadPhoto(fileName);
+	photo.status = 'fulfilled';
+	photo.value = upload;
+    }
+    catch (errror){
+	photo.status = 'rejected';
+	photo.value = error.toString();
+    }
+    try{
+	const signUp = await signUpUser(firstName, lastName);
+	user.status = 'fulfilled';
+	user.value = signUp;
+    }catch (error){
+	user.status = 'rejected';
+	user.value = error.toString();
+    }
+    return [user, photo];
 }
