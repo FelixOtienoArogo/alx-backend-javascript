a8f56c1295d155ad848971cb9e1ebb4fc4c28990
FelixOtienoArogo <felixarogo@gmail.com>
2023-04-20 12:33:17 +0300

    fixing lint errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,31 +2,30 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-    const photo = {
-	status: 'pending',
-	value: '',
-    }
-    const user = {
-	status: 'pending',
-	value: '',
-    }
+  const photo = {
+    status: 'pending',
+    value: '',
+  };
+  const user = {
+    status: 'pending',
+    value: '',
+  };
 
-    try {
-	const upload =	await uploadPhoto(fileName);
-	photo.status = 'fulfilled';
-	photo.value = upload;
-    }
-    catch (errror){
-	photo.status = 'rejected';
-	photo.value = error.toString();
-    }
-    try{
-	const signUp = await signUpUser(firstName, lastName);
-	user.status = 'fulfilled';
-	user.value = signUp;
-    }catch (error){
-	user.status = 'rejected';
-	user.value = error.toString();
-    }
-    return [user, photo];
+  try {
+    const upload = await uploadPhoto(fileName);
+    photo.status = 'fulfilled';
+    photo.value = upload;
+  } catch (error) {
+    photo.status = 'rejected';
+    photo.value = error.toString();
+  }
+  try {
+    const signUp = await signUpUser(firstName, lastName);
+    user.status = 'fulfilled';
+    user.value = signUp;
+  } catch (error) {
+    user.status = 'rejected';
+    user.value = error.toString();
+  }
+  return [user, photo];
 }
