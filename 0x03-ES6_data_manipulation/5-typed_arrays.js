0d33460bf2d2617326f3cbdc27508dfdc3aed31e
FelixOtienoArogo <felixarogo@gmail.com>
2023-04-25 10:45:41 +0300

    including error catch in 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,9 @@
-export default function (length, position, value) {
+export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer, 0, length);
-  try {
-    view.setInt8(position, value);
-  } catch (error) { Error('Position outside range'); }
+  if (position >= length) {
+    throw Error('Position outside range');
+  }
+  view.setInt8(position, value);
   return view;
 }
