02bea8be7e7bfd61fa702c93516901a3501ae5ec
FelixOtienoArogo <felixarogo@gmail.com>
2023-04-25 11:04:36 +0300

    fixing 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,9 @@ export const weakMap = new WeakMap();
 export function queryAPI(endPoint) {
   if (weakMap.has(endPoint)) {
     const number = weakMap.get(endPoint);
-    if (number >= 5) throw new Error('Endpoint load is high');
+    if (number >= 5) {
+      throw new Error('Endpoint load is high');
+    }
     weakMap.set(endPoint, weakMap.get(endPoint) + 1);
   } else {
     weakMap.set(endPoint, 1);
