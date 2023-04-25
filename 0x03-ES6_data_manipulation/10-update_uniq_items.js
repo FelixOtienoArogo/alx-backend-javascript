e6d0d312b22e89a560847520ba414365f1dc21e8
FelixOtienoArogo <felixarogo@gmail.com>
2023-04-25 10:55:10 +0300

    fixing error catch in 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,8 @@
 export default function updateUniqueItems(groc) {
   const update = groc;
+  if (!(groc instanceof Map)) {
+    throw Error('Cannot process');
+  }
   try {
     update.forEach((value, key) => {
       if (value === 1) {
