1c4dcc81c521dc344212ff9f5dab47a5bc321bdc
FelixOtienoArogo <felixarogo@gmail.com>
2023-04-26 08:59:18 +0300

    change limits in 10

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endPoint) {
   if (weakMap.has(endPoint)) {
     const number = weakMap.get(endPoint);
-    if (number >= 5) {
+    if (number >= 4) {
       throw new Error('Endpoint load is high');
     }
     weakMap.set(endPoint, weakMap.get(endPoint) + 1);
