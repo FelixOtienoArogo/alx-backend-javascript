ccd78e06c2e14a729a92d758381de7af087a6a96
FelixOtienoArogo <felixarogo@gmail.com>
2023-06-14 04:58:42 +0300

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,40 +1,50 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const rows = data.toString('utf-8').trim().split('\n');
-    const size = rows.length - 1;
-    const csvData = rows.map((row) => row.split(','));
-    const fields = {};
-    let keys = Object.keys(fields);
-    console.log(`Number of students: ${size}`);
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const rows = data.toString('utf-8').trim().split('\n');
+        const size = rows.length - 1;
+        const csvData = rows.map((row) => row.split(','));
+        const fields = {};
+        let message = '';
+        let keys = Object.keys(fields);
 
-    for (let i = 1; i < size + 1; i += 1) {
-      if (!keys.includes(csvData[i][3])) {
-        fields[csvData[i][3]] = 0;
+        const display = async () => {
+          console.log(`Number of students: ${size}`);
+          message += `Number of students: ${size}\n`;
+
+          for (let i = 1; i < size + 1; i += 1) {
+            if (!keys.includes(csvData[i][3])) {
+              fields[csvData[i][3]] = 0;
+            }
+            keys = Object.keys(fields);
+            if (keys.includes(csvData[i][3])) {
+              fields[csvData[i][3]] += 1;
+            }
+          }
+          let name = [];
+          for (let j = 0; j < keys.length; j += 1) {
+            for (let i = 1; i < size + 1; i += 1) {
+              if (keys[j] === csvData[i][3]) {
+                name.push(csvData[i][0]);
+              }
+            }
+            const string = name.map(String).join(', ');
+            console.log(`Number of students in ${keys[j]}: ${fields[keys[j]]}. List: ${string}`);
+            message += `Number of students in ${keys[j]}: ${fields[keys[j]]}. List: ${string}`;
+            name = [];
+          }
+        };
+        display();
+        resolve(message);
       }
-      keys = Object.keys(fields);
-      if (keys.includes(csvData[i][3])) {
-        fields[csvData[i][3]] += 1;
-      }
-    }
-    let name = [];
-    for (let j = 0; j < keys.length; j += 1) {
-      for (let i = 1; i < size + 1; i += 1) {
-        if (keys[j] === csvData[i][3]) {
-          name.push(csvData[i][0]);
-        }
-      }
-      const string = name.map(String).join(', ');
-      process.stdout.write(`Number of students in ${keys[j]}: ${fields[keys[j]]}.`);
-      process.stdout.write(' List: ');
-      console.log(string);
-      name = [];
-    }
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+    });
+  });
+  return promise;
 }
 
 module.exports = countStudents;
