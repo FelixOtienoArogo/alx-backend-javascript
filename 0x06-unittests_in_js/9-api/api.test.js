dbd2f411fd5d6fc88ec87f3932faaaac768540d2
FelixOtienoArogo <felixarogo@gmail.com>
2023-06-15 17:05:37 +0300

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe(`Index page`, () => {
+ it(`GET /`, (done) => {
+  const call = {
+	  url: 'http://localhost:7865',
+	  method: 'GET',
+  };
+  request(call, (error, response, body) => {
+   expect(response.statusCode).to.equal(200);
+   expect(body).to.equal('Welcome to the payment system');
+   done();
+  });
+ });
+
+ it(`GET /cart/:id(number)`, (done) => {
+  const call = {
+          url: 'http://localhost:7865/cart/10',
+          method: 'GET',
+  };
+  request(call, (error, response, body) => {
+   expect(response.statusCode).to.equal(200);
+   expect(body).to.equal('Payment methods for cart :10');
+   done();
+  });
+ });
+
+ it(`GET /cart/:id(not number)`, (done) => {
+  const call = {
+          url: 'http://localhost:7865/cart/number',
+          method: 'GET',
+  };
+  request(call, (error, response, body) => {
+   expect(response.statusCode).to.equal(404);
+   done();
+  });
+ });
+});
