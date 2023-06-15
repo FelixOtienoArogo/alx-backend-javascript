3ee942a8b7a1c609def487c76a2c533072ded06b
FelixOtienoArogo <felixarogo@gmail.com>
2023-06-15 11:57:36 +0300

    Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,14 @@
+const sinon = require('sinon');
+const assert = require('assert');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('validate the usage of the Utils function', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+
+    assert(spy.calledOnce);
+    spy.restore();
+  });
+});
