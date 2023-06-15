394e3e853e3973f76cdb13a575791eede0eb20eb
FelixOtienoArogo <felixarogo@gmail.com>
2023-06-15 10:15:40 +0300

    fix lint

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,16 +1,18 @@
 function calculateNumber(type, a, b) {
+  let result = 0;
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
   }
-	 if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
+  if (type === 'SUBTRACT') {
+    result = Math.round(a) - Math.round(b);
   }
-	 if (type === 'DIVIDE') {
-		 if (b === 0) {
+  if (type === 'DIVIDE') {
+    if (b === 0) {
       return ('Error');
-		 }
-    return Math.round(a) / Math.round(b);
+    }
+    result = Math.round(a) / Math.round(b);
   }
+  return result;
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,81 +2,81 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-	describe('SUM', () => {
-  it('should test for integers', () => {
-    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+  describe('sUM', () => {
+    it('should test for integers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+    });
+
+    it('should test for int and float', () => {
+      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+    });
+
+    it('should test for rounding down', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+    });
+
+    it('should test for rounding down', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+    });
+
+    it('should test for zero', () => {
+      assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
+    });
+
+    it('should test for negative', () => {
+      assert.strictEqual(calculateNumber('SUM', -1.6, -1.7), -4);
+    });
   });
 
-  it('should test for int and float', () => {
-    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+	        describe('sUBTRACT', () => {
+    it('should test for integers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 3, 1), 2);
+    });
+
+    it('should test for int and float', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
+    });
+
+    it('should test for rounding down', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+    });
+
+    it('should test for rounding down', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+    });
+
+    it('should test for zero', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 0, 0), 0);
+    });
+
+    it('should test for negative', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', -1.6, -1.7), 0);
+    });
   });
 
-  it('should test for rounding down', () => {
-    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
-  });
+	        describe('dIVIDE', () => {
+    it('should test for integers', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 4, 2), 2);
+    });
 
-  it('should test for rounding down', () => {
-    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
-  });
+    it('should test for int and float', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 3.7), 0.25);
+    });
 
-  it('should test for zero', () => {
-    assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
-  });
+    it('should test for rounding down', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.2, 3.7), 0.25);
+    });
 
-  it('should test for negative', () => {
-    assert.strictEqual(calculateNumber('SUM', -1.6, -1.7), -4);
-  });
-	});
+    it('should test for rounding down', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
+    });
 
-	        describe('SUBTRACT', () => {
-  it('should test for integers', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 3, 1), 2);
-  });
+    it('should test for zero', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 0, 0), 'Error');
+    });
 
-  it('should test for int and float', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
+    it('should test for negative', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', -1.6, -1.7), 1);
+    });
   });
-
-  it('should test for rounding down', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
-  });
-
-  it('should test for rounding down', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
-  });
-
-  it('should test for zero', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 0, 0), 0);
-  });
-
-  it('should test for negative', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', -1.6, -1.7), 0);
-  });
-        });
-
-	        describe('DIVIDE', () => {
-  it('should test for integers', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 4, 2), 2);
-  });
-
-  it('should test for int and float', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1, 3.7), 0.25);
-  });
-
-  it('should test for rounding down', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.2, 3.7), 0.25);
-  });
-
-  it('should test for rounding down', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
-  });
-
-  it('should test for zero', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 0, 0), 'Error');
-  });
-
-  it('should test for negative', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', -1.6, -1.7), 1);
-  });
-        });
 });
